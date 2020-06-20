94286a9b8cacf8811eb118c5ba4aa69bc2ca4b8d
MemetAkif <67182475+MemetAkif@users.noreply.github.com>
2020-06-20 15:17:38 +0200

    Update YunusClass.java

--- src/YunusClass.java
+++ src/YunusClass.java
@@ -1,6 +1,7 @@
 public class YunusClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
+         System.out.println("Gittub dan merhaba");
     }
 
 }
